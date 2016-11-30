0116fdd734a29d40b403683856de21ea07ece58a
Sara <saramurray@protonmail.com>
2016-11-30 03:23:23 -0800

    User credentials are now saved

--- tootstream/toot.py
+++ tootstream/toot.py
@@ -1,15 +1,19 @@
 import os.path
 import click
+import readline
+import getpass
 from mastodon import Mastodon
 
 # TODO: need to modify this to support multiple shards, since we have to register per shard
-# i think
-APP_PATH = 'file.txt'
-
+# For now it only supports the default mastodon.social shard
+APP_PATH = os.path.expanduser('~/.config/tootstream/client.txt')
+APP_CRED = os.path.expanduser('~/.config/tootstream/token.txt')
 USER_REGEX = "[a-zA-Z0-9_]{1,30}"
 
 
 def register_app():
+    if not os.path.exists(os.path.expanduser('~/.config/tootstream')):
+        os.makedirs(os.path.expanduser('~/.config/tootstream'))
     if os.path.isfile(APP_PATH):
         return
     Mastodon.create_app(
@@ -18,27 +22,70 @@ def register_app():
     )
 
 
-def login(name, password, shard=None):
+def login(mastodon, name, password, shard=None):
     """
     Login to Mastodon shard.
     Return a Mastodon client if login success, otherwise returns None.
     """
-    mastodon = Mastodon(client_id=APP_PATH)
     mastodon.log_in(
         name,
         password,
-        to_file = 'tootstream_usercred.txt'
+        to_file = APP_CRED
     )
-    return mastodon
 
+commands = {}
+def command(func):
+    commands[func.__name__] = func
+    return func
+
+@command
+def toot(mastodon, rest):
+    """Publish a toot. eg 'toot hello world' will publish 'hello world'"""
+    mastodon.toot(rest)
+
+@command
+def help(mastodon, rest):
+    """List all commands."""
+    print("Commands:")
+    for command, cmd_func in commands.items():
+        print("\t{}\t{}".format(command, cmd_func.__doc__))
+
+def authenticated(mastodon):
+    if not os.path.isfile(APP_CRED):
+        return False
+    if mastodon.account_verify_credentials().get('error'):
+        return False
+    return True
 
 @click.command()
-@click.option('--name', prompt=True)
-@click.option('--password', prompt=True, hide_input=True)
+@click.option('--name')
+@click.option('--password')
 def main(name, password):
     register_app()
-    mastodon = login(name, password)
-    mastodon.toot("")
+
+    mastodon = Mastodon(client_id=APP_PATH, access_token=APP_CRED)
+
+    if name and password:
+        login(mastodon, name, password)
+    elif not authenticated(mastodon):
+        name = input("Name: ")
+        password = getpass.getpass()
+        login(mastodon, name, password)
+
+    say_error = lambda a, b: print("Invalid command. Use 'help' for a list of commands")
+
+    print("Welcome to Tootstream")
+    print("Enter a command. Use 'help' for a list of commands")
+    while True:
+        command = input("> ").split(' ', 1)
+        rest = ""
+        try:
+            rest = command[1]
+        except IndexError:
+            pass
+        command = command[0]
+        cmd_func = commands.get(command, say_error)
+        cmd_func(mastodon, rest)
 
 if __name__ == '__main__':
     main()
