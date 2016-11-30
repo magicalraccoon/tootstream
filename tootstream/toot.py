4f6e05e2fcc34d2464e8033bd2686857bd7f5f11
Sara <saramurray@protonmail.com>
2016-11-29 19:59:12 -0800

    Logging in works

--- tootstream/toot.py
+++ tootstream/toot.py
@@ -1,12 +1,15 @@
 import os.path
+import click
 from mastodon import Mastodon
 
+# TODO: need to modify this to support multiple shards, since we have to register per shard
+# i think
 APP_PATH = 'file.txt'
 
 USER_REGEX = "[a-zA-Z0-9_]{1,30}"
 
 
-def check_file():
+def register_app():
     if os.path.isfile(APP_PATH):
         return
     Mastodon.create_app(
@@ -15,9 +18,27 @@ def check_file():
     )
 
 
-def main():
-    pass
+def login(name, password, shard=None):
+    """
+    Login to Mastodon shard.
+    Return a Mastodon client if login success, otherwise returns None.
+    """
+    mastodon = Mastodon(client_id=APP_PATH)
+    mastodon.log_in(
+        name,
+        password,
+        to_file = 'tootstream_usercred.txt'
+    )
+    return mastodon
 
+
+@click.command()
+@click.option('--name', prompt=True)
+@click.option('--password', prompt=True, hide_input=True)
+def main(name, password):
+    register_app()
+    mastodon = login(name, password)
+    mastodon.toot()
 
 if __name__ == '__main__':
     main()
