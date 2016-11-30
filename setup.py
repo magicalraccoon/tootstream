163825fc1f5419b289faab2aa89c57e286a5c71b
Sara <magicalraccoon@users.noreply.github.com>
2016-11-30 03:30:45 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name="TootStream",
+    name="tootstream",
     version="0.1",
     packages=find_packages(),
     scripts=['tootstream.py'],
