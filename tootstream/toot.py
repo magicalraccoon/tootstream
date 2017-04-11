323bf03475c55fd54e7de63624101266aee6b13d
Claire Cavanaugh <dauxra@gmail.com>
2017-04-11 15:50:38 -0700

    Implemented a class for the ID map

--- tootstream/toot.py
+++ tootstream/toot.py
@@ -16,6 +16,31 @@ html_parser = HTMLParser()
 
 COLORS = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
 
+class IdDict:
+    """Represents a mapping of local (tootstream) ID's to global
+    (mastodon) IDs."""
+    def __init__(self):
+        self._map = []
+
+    def to_local(self, global_id):
+        """Returns the local ID for a global ID"""
+        global_id = int(global_id) # In case a string gets passed
+        try:
+            return self._map.index(global_id)
+        except ValueError:
+            self._map.append(global_id)
+            return len(self._map) - 1
+
+    def to_global(self, local_id):
+        """Returns the global ID for a local ID"""
+        local_id = int(local_id) 
+        try:
+            return self._map[local_id]
+        except:
+            tprint('Invalid ID.', 'red', '')
+        
+IDS = IdDict();
+
 
 def parse_config():
     if not os.path.exists(CONF_PATH):
@@ -187,6 +212,7 @@ def home(mastodon, rest):
 
         cprint(reblogs_count, 'cyan', end="")
         cprint(favourites_count, 'yellow', end="")
+        
         cprint("id:" + toot_id, 'red')
 
 
