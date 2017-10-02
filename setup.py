acadd5934b9a359f69b81e460d6fa4f791da2493
Craig Maloney <craig@decafbad.net>
2017-10-02 10:04:45 -0400

    Removing comments from setup.py

--- setup.py
+++ setup.py
@@ -9,17 +9,15 @@ setup(
     package_data={
     },
 
-    # metadata for upload to PyPI
     author="Sara Murray",
     author_email="saramurray@protonmail.com",
     description="A command line interface for interacting with Mastodon instances",
     license="MIT",
     keywords="mastodon, mastodon.social, toot, tootstream",
-    url="http://www.github.com/magicalraccoon/tootstream",   # project home page, if any
+    url="http://www.github.com/magicalraccoon/tootstream",
     entry_points={
         'console_scripts':
             ['tootstream=tootstream.toot:main']
     }
 
-    # could also include long_description, download_url, classifiers, etc.
 )
