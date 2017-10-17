4214e409b010c1ad5bce94c839f895db5bc9b74f
Craig Maloney <craig@decafbad.net>
2017-10-17 10:20:30 -0400

    Fixing package issues for setup.py develop

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     install_requires=[line.strip() for line in open('requirements.txt')],
 
     packages=find_packages('src'),
-    package_dir={'tootstream': 'src/tootstream'}, include_package_data=True,
+    package_dir={'': 'src'}, include_package_data=True,
     package_data={
     },
 
